e8294b794341d3a3fae9ff68efda06cc03b78e93
Marlon Santos <82154609+marloon-mon@users.noreply.github.com>
2025-09-24 12:25:18 -0300

    feat: classe Biblioteca e método construstor e seus atributos

--- biblioteca.py
+++ biblioteca.py
@@ -1,3 +1,10 @@
 from usuario import Usuario
 from livro import Livro
 from dados import Dados
+
+class Biblioteca:
+    _arquivo = "biblioteca"
+
+    def __init__(self):
+        self.usuarios = []
+        self.livros = []
