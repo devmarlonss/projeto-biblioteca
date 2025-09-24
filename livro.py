be70a7b5bdcf99a53c6b4d9a1214ceddd8c264b2
Marlon Santos <82154609+marloon-mon@users.noreply.github.com>
2025-09-24 12:20:47 -0300

    feat: métodos getters dos atributos da classe livro(titulo, ano, autor, genero, disponivel)

--- livro.py
+++ livro.py
@@ -7,3 +7,23 @@ class Livro:
         self.__autor = autor
         self.__genero = genero
         self.__disponivel = disponivel
+
+    @property
+    def titulo(self):
+        return self.__titulo
+    
+    @property
+    def ano(self):
+        return self.__ano
+    
+    @property
+    def autor(self):
+        return self.__autor
+    
+    @property
+    def genero(self):
+        return self.__genero
+    
+    @property
+    def disponivel(self):
+        return self.__disponivel
